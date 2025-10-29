6898ff68406d5954fc9ed5cfd6c3d605199ea822
Natalie <134658432+NatalieNike@users.noreply.github.com>
2025-10-29 18:24:13 -0300

    Terminando API
    adicionado, consulta, criação, atualização e exclusão de livros.
    testes concluídos.

--- API_livros.py
+++ API_livros.py
@@ -1,8 +1,7 @@
 #API é um lugar para disponibilizar recursos e/ou funcionalidades
 #1. Objetivo - Criar um api que disponibiliza a consulta, criação, edição e exclusão de livros.
 #2. URL base - localhost.com
-#3. Endpoints - (localhost/livros - GET), (localhost/livros - POST), (localhost/livros/id - GET), (localhost/livros/id - PUT), (localhost/livros/id - DELETE)
-
+#3. Endpoints - (localhost/livros - GET), (localhost/livros - POST), (localhost/livros/id - PUT), (localhost/livros/id - DELETE)
 
 
 from flask import Flask, jsonify, request
@@ -37,14 +36,14 @@ livros = [
     }
 ]
 
-# Consultar todos - GET
+# Consultar todos - GET (Read)
 #Em Flask, o @app.route() é um decorator que diz ao framework:
 #“Quando alguém acessar essa URL, execute essa função.”
 @app.route('/livros', methods=['GET'])
 def obter_livros():
     return jsonify(livros)
 
-# Consultar por ID - GET
+# Consultar por ID - GET (Read)
 @app.route('/livros/<int:id>', methods=['GET'])
 def obter_livro_por_id(id):
     for livro in livros: 
@@ -52,6 +51,34 @@ def obter_livro_por_id(id):
             return jsonify(livro)
     return jsonify({'mensagem': 'Livro não encontrado'}), 404
 
-# Editar
-# Excluir
+# Criar novo livro - POST (Create)
+@app.route('/livros', methods=['POST'])
+def adicionar_livro():
+    novo_livro = request.get_json()
+    livros.append(novo_livro)
+    return jsonify(livros), 201
 
+# Atuallizar um livro existente - PUT (Update)
+@app.route('/livros/<int:id>', methods=['PUT'])
+def editar_livro(id):
+    dados_atualizados = request.get_json()
+    for livro in livros:
+        if livro['id'] == id:
+            # Atualiza apenas os campos título e autor (mantém o id)
+            livro['título'] = dados_atualizados.get('título', livro['título'])
+            livro['autor'] = dados_atualizados.get('autor', livro['autor'])
+            return jsonify(livro)
+    return jsonify({'mensagem': 'Livro não encontrado'}), 404
+
+    
+
+# Excluir um livro existente - DELETE (Delete)
+@app.route('/livros/<int:id>', methods=['DELETE'])
+def excluir_livro(id):
+    for indice, livro in enumerate(livros): 
+        if livro['id'] == id:
+            del livros[indice]
+            return jsonify({'mensagem': 'Livro excluído com sucesso!'}), 404
+        
+if __name__ == '__main__':
+    app.run(debug=True)
